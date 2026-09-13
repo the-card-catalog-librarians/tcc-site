7a1e8c056ef4715a8fc6e1aac523dfad12f9cb00
michael-masarik <masarikfamilymichael@gmail.com>
2026-09-12 21:41:12 -0500

    Added google analytics script

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,21 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx(), react(), sitemap()],
+  integrations: [mdx(), react(), sitemap(), {
+    name: "ga-injector",
+    hooks: {
+      "astro:config:setup": ({injectScript})=>{
+        injectScript("head-inline",`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-T106EFK72F');`);
+          injectScript('head-inline',`var s = document.createElement('script');
+              s.async = true;
+              s.src = 'https://www.googletagmanager.com/gtag/js?id=G-T106EFK72F';
+              document.head.appendChild(s);`)
+      }
+    }
+  }],
   site: "https://thecardcataloglibrarians.com",
 })
